fefc134a5ee9c07c264793b3ea3e7768c71b9a40
enjarai <enjarai@trans.fish>
2026-03-22 15:17:52 +0100

    the meow

--- src/client/java/dev/enjarai/trickster/render/fragment/ColorRenderer.java
+++ src/client/java/dev/enjarai/trickster/render/fragment/ColorRenderer.java
@@ -1,6 +1,7 @@
 package dev.enjarai.trickster.render.fragment;
 
 import dev.enjarai.trickster.Trickster;
+import dev.enjarai.trickster.render.CircleRenderer;
 import dev.enjarai.trickster.render.SpellCircleRenderer;
 import dev.enjarai.trickster.spell.fragment.ColorFragment;
 import net.minecraft.client.render.*;
@@ -23,15 +24,15 @@ public class ColorRenderer implements FragmentRenderer<ColorFragment> {
     static float lastDt = 0;
 
     @Override
-    public void render(ColorFragment fragment, MatrixStack matrices, VertexConsumerProvider vertexConsumers, float x, float y, float size, float alpha, Vec3d normal, float tickDelta,
-            SpellCircleRenderer delegator) {
+    public void render(ColorFragment fragment, MatrixStack matrices, VertexConsumerProvider vertexConsumers, float x, float y, float radius, float alpha, Vec3d normal, float tickDelta,
+        CircleRenderer delegator) {
         var col = fragment.color();
         float time = System.currentTimeMillis() % 1048576 / 50f;
-        size /= 4;
+        radius /= 4;
         int r = getRed(col), g = getGreen(col), b = getBlue(col), a = getAlpha(col);
         matrices.push();
         matrices.translate(x, y, 0);
-        matrices.scale(size, size, delegator.inUI ? size : -size);
+        matrices.scale(radius, radius, delegator.inUI ? radius : -radius);
         for (int i = 0; i < 8; i++) {
             var rand = new Random(((int) (time / 5) + i) * 1000);
             var rotv = rand.nextFloat() - 0.5f;
@@ -46,20 +47,20 @@ public class ColorRenderer implements FragmentRenderer<ColorFragment> {
             matrices.multiply(new Quaternionf(new AxisAngle4f(rot + age * rotv, 0, 0, 1)));
             matrices.scale(scale, scale, 1);
             SpellCircleRenderer.drawTexturedQuad(
-                    matrices, vertexConsumers, TEXTURE,
-                    -1, 1, -1, 1,
-                    0.1f - age * 0.01f,
-                    MathHelper.lerp(fade, r / 255f, 1),
-                    MathHelper.lerp(fade, g / 255f, 1),
-                    MathHelper.lerp(fade, b / 255f, 1),
-                    alpha * delegator.getCircleTransparency() * a / 255f, delegator.inUI
+                matrices, vertexConsumers, TEXTURE,
+                -1, 1, -1, 1,
+                0.1f - age * 0.01f,
+                MathHelper.lerp(fade, r / 255f, 1),
+                MathHelper.lerp(fade, g / 255f, 1),
+                MathHelper.lerp(fade, b / 255f, 1),
+                alpha * delegator.getCircleTransparency() * a / 255f, delegator.inUI
             );
             SpellCircleRenderer.drawTexturedQuad(
-                    matrices, vertexConsumers, BACK_TEXTURE,
-                    -1, 1, -1, 1,
-                    0.05f - age * 0.01f,
-                    1, 1, 1,
-                    alpha * delegator.getCircleTransparency(), delegator.inUI
+                matrices, vertexConsumers, BACK_TEXTURE,
+                -1, 1, -1, 1,
+                0.05f - age * 0.01f,
+                1, 1, 1,
+                alpha * delegator.getCircleTransparency(), delegator.inUI
             );
             matrices.pop();
         }
